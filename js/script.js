44097f4e95046a104a23149e8f4aee2e7c513c22
FedericaC03 <fdrk03@hotmail.it>
2020-08-06 15:11:41 +0200

    Esercizio da completare

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+//CHIEDO ALL'UTENTE DI INSERIRE IL SUO NOME
+var nome = prompt('Inserisci il tuo nome');
 
+//CHIEDO ALL'UTENTE DI INSERIRE IL SESSO
+var sesso = prompt('Inserisci il tuo sesso: f o m');
+
+//METTO IL MIO SPAN IN UNA VARIABILE E AGGIUNGO UNA CLASSE
+var nomeutente = document.getElementById('nome-utente');
+
+if (sesso == "f") {
+   nomeutente.innerHTML = nome;
+   nomeutente.style.color = "pink";
+ } else if (sesso == "m"){
+   nomeutente.innerHTML = nome;
+   nomeutente.style.color = "blue";
+} else {
+  alert('Attenzione stai sbagliando a scrivere il sesso')
+}
